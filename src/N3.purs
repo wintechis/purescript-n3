e14dbfb59db153d7822fae07b026de451fbf6c61
Daniel Schraudner <daniel.schraudner@fau.de>
2022-12-13 13:24:55 +0100

    Add documentation comment

--- src/N3.purs
+++ src/N3.purs
@@ -16,10 +16,12 @@ data Format = Turtle | TriG | NTriples | NQuads
 
 foreign import parseImpl :: String -> Format -> String -> Effect (Promise (Array Quad))
 
+-- | Given a base URI and an RDF serialization format, parses a given string.
 parse :: String -> Format -> String -> Aff (Array Quad)
 parse base format = (parseImpl base format) >>> toAffE
 
 foreign import writeImpl :: String -> Format -> Array Quad -> Effect (Promise String)
 
+-- | Given a base URI and an RDF serialization format, serializes an array of quads.
 write :: String -> Format -> Array Quad -> Aff String
 write base format = (writeImpl base format) >>> toAffE
